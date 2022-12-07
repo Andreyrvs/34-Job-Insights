c98e09c6d9dff4ba40c3dd77f2fdeb3c2def8fc6
Andrey Visniewski <cortaesai@gmail.com>
2022-12-07 10:54:00 -0300

    chore: (Insights) Jobs - filtra as industrias

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -18,18 +18,9 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
+    filtered_industry = []
+    for industries in jobs:
+        if industries["industry"] == industry:
+            filtered_industry.append(industries)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return filtered_industry
