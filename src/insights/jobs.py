e19fdf7a809503345fa6f2bfa42b2b3886555390
Andrey Visniewski <cortaesai@gmail.com>
2023-02-19 16:15:14 -0300

    Feat: Ferramenta de limpeza Trybe

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -5,6 +5,7 @@ import csv
 
 @lru_cache
 def read(path: str) -> List[Dict]:
+    print(path)
     with open(path, encoding="utf-8") as file:
 
         jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
